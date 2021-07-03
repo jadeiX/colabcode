88060e235fd4bf0bc92a030fda349c503311f7fc
Jade <32917209+jadeiX@users.noreply.github.com>
2021-07-03 12:38:04 -0700

    Update code.py

--- colabcode/code.py
+++ colabcode/code.py
@@ -28,9 +28,6 @@ EXTENSIONS = [
     "oderwat.indent-rainbow",
     "ms-python.vscode-pylance",
     "pkief.material-icon-theme",
-    "littlefoxteam.vscode-python-test-adapter",
-    "ms-vscode.test-adapter-converter",
-    "hbenl.vscode-test-explorer"
 ]
 CODESERVER_VERSION = "3.10.2"
 
